e4f94608859ff0e085fcd9f629ee7e1f390e1342
shabaka computers <aadilvazhakkad@gmail.com>
2023-04-14 17:13:41 +0530

    Implemented polymorphism_2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -12,19 +12,7 @@ public class Main {
         System.out.print(a+" "+b+" "+c);
     }
 }
-    class Product {
-        public int product(int x, int y) {
-            return x + y;
-        }
 
-        public int product(int x, int y, int z) {
-            return x + y + z;
-        }
-
-        public double product(double x, double y) {
-            return x + y;
-        }
-    }
 
 
 
