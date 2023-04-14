b8ace56c56f1d0f7ec7e198068027dd71e03c903
shabaka computers <aadilvazhakkad@gmail.com>
2023-04-14 17:37:01 +0530

    Implemented polymorphism_4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,8 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        Product p = new Main().new Product();
+        Main m= new Main();
+        Main.Product p = m.new Product();
         int a = p.product(5, 6);
 
         int b = p.product(1, 2, 3);
