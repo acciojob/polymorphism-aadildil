bdf694c5e3edf0f7d860271e00d580236836b9b5
shabaka computers <aadilvazhakkad@gmail.com>
2023-04-14 17:10:11 +0530

    Implemented polymorphism_2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,23 +3,28 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        Product p=new Product();
-        int a=p.product(5,6);
-        int b=p.product(1,2,3);
-        double c=p.product(10,20);
+        Product p = new Product();
+        int a = p.product(5, 6);
+
+        int b = p.product(1, 2, 3);
+        double c = p.product(10, 20);
+
+        System.out.print(a+" "+b+" "+c);
     }
 }
+    class Product {
+        public int product(int x, int y) {
+            return x + y;
+        }
 
- class Product {
-    public int product(int x, int y) {
-        return x + y;
+        public int product(int x, int y, int z) {
+            return x + y + z;
+        }
+
+        public double product(double x, double y) {
+            return x + y;
+        }
     }
 
-    public int product(int x, int y, int z) {
-        return x + y + z;
-    }
 
-    public double product(double x, double y) {
-        return x + y;
-    }
-}
+
